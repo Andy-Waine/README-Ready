b814e579474f1e0f99f392132d7b2f2610f25325
Alabama Whiskey <amwtablet@gmail.com>
2021-11-30 20:34:41 -0500

    promptUser function added

--- index.js
+++ index.js
@@ -3,3 +3,33 @@ const fs = require("fs");
 const util = require("util");
 const inquirer = require("inquirer");
 const generateReadme = require("./utils/generateReadme")
+const writeFileAsync = util.promisify(fs.writeFile);
+
+
+function promptUser(){
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "projectTitle",
+            message: "What is the project title?",
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Write a brief description of your project: "
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "Describe the installation process if any: ",
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "What is this project usage for?"
+        }
+    ]);
+} 
+
+
+  
